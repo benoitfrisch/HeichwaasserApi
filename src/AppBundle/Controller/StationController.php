2b705e9c5039c4ef536dc083790292686905a3f1
Benoît Frisch <benoit@fresh.lu>
2018-01-29 19:26:00 +0100

    Add /reversed option (=Ascending order of timestamps)

--- src/AppBundle/Controller/StationController.php
+++ src/AppBundle/Controller/StationController.php
@@ -50,6 +50,33 @@ class StationController extends Controller
     /**
      * @ApiDoc(
      *  resource=true,
+     *  description="This displays all a station with measurements."
+     * )
+     * @Get("/api/v1/stations/{id}/reversed", defaults={"_format"="json"})
+     * @View(serializerGroups={"station","river","station_river","measurement", "alert","oneMeasurement"})
+     */
+    public
+    function getRiverDetailOrderAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $station = $em->getRepository('AppBundle:Station')->find($id);
+
+        $measurements = $em
+            ->getRepository(Measurement::class)->createQueryBuilder('a')
+            ->where('a.station = :station')
+            ->orderBy('a.timestamp', 'ASC')
+            ->setParameter('station', $station)
+            ->getQuery()
+            ->getResult();
+
+        $station->setMeasurements(new ArrayCollection($measurements));
+
+        return $station;
+    }
+
+    /**
+     * @ApiDoc(
+     *  resource=true,
      *  description="This displays all a station with measurements between the start and end date."
      * )
      * @Get("/api/v1/stations/{id}/limit/{limit}", defaults={"_format"="json"})
@@ -80,11 +107,77 @@ class StationController extends Controller
      *  resource=true,
      *  description="This displays all a station with measurements between the start and end date."
      * )
+     * @Get("/api/v1/stations/{id}/limit/{limit}/reversed", defaults={"_format"="json"})
+     * @View(serializerGroups={"station","river","station_river","measurement", "alert","oneMeasurement"})
+     */
+    public
+    function getRiverDetailLimitOrderAction($id, $limit)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $station = $em->getRepository('AppBundle:Station')->find($id);
+
+        $measurements = $em
+            ->getRepository(Measurement::class)->createQueryBuilder('a')
+            ->where('a.station = :station')
+            ->orderBy('a.timestamp', 'ASC')
+            ->setParameter('station', $station)
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult();
+
+        $station->setMeasurements(new ArrayCollection($measurements));
+
+        return $station;
+    }
+
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="This displays all a station with measurements between the start and end date."
+     * )
      * @Get("/api/v1/stations/{id}/start/{start}/end/{end}", defaults={"_format"="json"})
      * @View(serializerGroups={"station","river","station_river","measurement", "alert","oneMeasurement"})
      */
     public
     function getRiverDetailDateRangeAction($id, $start, $end)
+    {
+        $startDate = DateTime::createFromFormat('U', $start);
+        $endDate = DateTime::createFromFormat('U', $end);
+
+        if ($startDate > $endDate) {
+            throw new HttpException(400, "Start date has to be before end date.");
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $station = $em->getRepository('AppBundle:Station')->find($id);
+
+        $measurements = $em
+            ->getRepository(Measurement::class)->createQueryBuilder('a')
+            ->where('a.station = :station')
+            ->andWhere('a.timestamp > :start')
+            ->andWhere('a.timestamp < :end')
+            ->orderBy('a.timestamp', 'ASC')
+            ->setParameter('start', $startDate)
+            ->setParameter('end', $endDate)
+            ->setParameter('station', $station)
+            ->getQuery()
+            ->getResult();
+
+        $station->setMeasurements(new ArrayCollection($measurements));
+
+        return $station;
+    }
+
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="This displays all a station with measurements between the start and end date."
+     * )
+     * @Get("/api/v1/stations/{id}/start/{start}/end/{end}/reversed", defaults={"_format"="json"})
+     * @View(serializerGroups={"station","river","station_river","measurement", "alert","oneMeasurement"})
+     */
+    public
+    function getRiverDetailDateRangeOrderAction($id, $start, $end)
     {
         $startDate = DateTime::createFromFormat('U', $start);
         $endDate = DateTime::createFromFormat('U', $end);
